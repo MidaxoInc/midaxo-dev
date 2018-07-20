d4b2fdcbbc7cff4865baa5311ce108eb8977e526
sagarvelagala <39137338+sagarvelagala@users.noreply.github.com>
2018-07-20 10:35:46 -0400

    update to replace 0 value deals with trailing asp

--- models/function/PIPELINE_DAILY.sql
+++ models/function/PIPELINE_DAILY.sql
@@ -3,21 +3,36 @@ with
   datespine as (select * from {{ref('DATETABLE_CLEAN')}}),
   dealtable as (select * from {{ref('DEAL')}}),
   dealhistory as (select * from {{ref('DEAL_ARCHIVE_CLEAN')}}),
-  pipelines as (select * from {{ref('PIPELINE_PROPERTY')}})
+  pipelines as (select * from {{ref('PIPELINE_PROPERTY')}}),
+  asp as (
+    select
+      d.ddate,
+      avg(a.deal_amount) as t180_asp
+    from datespine d
+    left join dealtable a
+      on a.closedate between dateadd('day',-180, d.ddate) and d.ddate
+    where
+      contains(a.pipeline_stage, 'won') = true
+      and a.pipeline_type = 'direct'
+    group by d.ddate
+  )
 
 select
-d.*,
-t.deal_id,
-t.deal_name,
-t.company_name,
-p.pipeline_type,
-p.pipeline_stage,
-p.pipeline_stageorder,
-h.deal_amount,
-h.createdate,
-h.closedate,
-h.validfrom,
-h.validto
+  d.*,
+  t.deal_id,
+  t.deal_name,
+  t.company_name,
+  p.pipeline_type,
+  p.pipeline_stage,
+  p.pipeline_stageorder,
+  case
+    when (p.pipeline_stage in ('qualification') or h.deal_amount = '0') then asp.t180_asp
+    else h.deal_amount
+  end as deal_amount,
+  h.createdate,
+  h.closedate,
+  h.validfrom,
+  h.validto
 from datespine d
 
 inner join dealtable t
