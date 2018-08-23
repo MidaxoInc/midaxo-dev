77f42ea26760f5308d7131363a027ca54fa83e86
sagarvelagala <39137338+sagarvelagala@users.noreply.github.com>
2018-08-23 18:26:20 -0400

    add usage table

---
+++ models/base/EVENT_USAGE.sql
@@ -0,0 +1,12 @@
+select
+a.time::timestamp_ntz as eventdate,
+a.distinct_id as user_id,
+a.name as usage_event,
+a.properties:"Company ID"::string as company_id,
+a.properties:"Process ID"::string as process_id,
+a.properties:"Process Level Role"::string as user_role,
+a.properties:"Tab Name"::string as navigation_tab,
+a.properties:"Sub-Nav Tab Name"::string as navigation_subtab,
+a.properties:"User Role"::string as adduser_role
+a.properties
+from raw.mixpanel.event a
