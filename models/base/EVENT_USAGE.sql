7fbc9fee29a5b3ebd6dba3b9047f5a9185748670
sagarvelagala <39137338+sagarvelagala@users.noreply.github.com>
2018-08-24 09:50:57 -0400

    add missing comma

--- models/base/EVENT_USAGE.sql
+++ models/base/EVENT_USAGE.sql
@@ -7,6 +7,6 @@ a.properties:"Process ID"::string as process_id,
 a.properties:"Process Level Role"::string as user_role,
 a.properties:"Tab Name"::string as navigation_tab,
 a.properties:"Sub-Nav Tab Name"::string as navigation_subtab,
-a.properties:"User Role"::string as adduser_role
+a.properties:"User Role"::string as adduser_role,
 a.properties
 from raw.mixpanel.event a
