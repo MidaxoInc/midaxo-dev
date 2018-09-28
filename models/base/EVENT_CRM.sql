efaea049a5c063026498036ae02506b8e8299f90
sagarvelagala <39137338+sagarvelagala@users.noreply.github.com>
2018-09-28 08:56:55 -0400

    update company to engagement

--- models/base/EVENT_CRM.sql
+++ models/base/EVENT_CRM.sql
@@ -2,15 +2,19 @@ with
   engagement as (
   select
     x.id,
-    y.contact_id as contact_id,
+    y.contact_id,
+    z.company_id,
     x.created_at::timestamp_ntz as eventdate,
     lower(x.type) as event_type,
     'na' as event_action,
     x.owner_id::varchar as event_owner_campaign_url
-  from RAW.HUBSPOT.ENGAGEMENT x
+  from RAW.HUBSPOT2.ENGAGEMENT x
 
-  left join RAW.HUBSPOT.ENGAGEMENT_CONTACT y
+  left join RAW.HUBSPOT2.ENGAGEMENT_CONTACT y
     on x.id = y.engagement_id
+
+  left join RAW.HUBSPOT2.ENGAGEMENT_COMPANY z
+    on x.id = z.engagement_id
   ),
   contact as (
     select *
@@ -24,8 +28,8 @@ with
 select distinct
   md5(e.id) as event_id,
   e.id::varchar as engagement_id,
-  c.id as contact_id,
-  c.property_associatedcompanyid as company_id,
+  e.contact_id,
+  e.company_id,
   e.eventdate,
   case
     when contains(e.event_type,'email') then 'sales_email'
@@ -38,8 +42,6 @@ select distinct
   e.event_owner_campaign_url::varchar as event_owner_campaign_url
 from engagement e
 
-left join contact c
-  on e.contact_id = c.id
 left join mstd m
   on m.owner_id = e.event_owner_campaign_url
   and last_day(m.ddate,'month') = last_day(e.eventdate,'month')
